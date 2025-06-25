564da2f4fd12eaddbdc85c4a77ee19c80ea03cda
Rafiur Rahman <bsrsoftbd@gmail.com>
2025-06-26 00:45:11 +0100

    - created design pattern

--- mobile_banking/lib/main.dart
+++ mobile_banking/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/theme/app_theme.dart';
-import 'ui/screens/home/home_screen.dart';
+import 'features/home/presentation/pages/home_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 void main() async {
