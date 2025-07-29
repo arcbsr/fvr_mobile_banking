31dc035218900444d5bc564b4d2c871a95746baf
mahfuz53 <mahfuz53@gmail.com>
2025-07-30 02:19:56 +0600

    Login. Sign up almost done, need to verification.

---
+++ mobile_banking/lib/core/utils/app_url.dart
@@ -0,0 +1,10 @@
+/**
+ * Created by Mahfuz
+ * 27th july 2025
+ **/
+class AppUrl {
+  static var baseUrl = 'https://197.140.18.10/api';
+  static var loginEndPint = '$baseUrl/api/v2/auth/login';
+  static var signUpEndPint = '$baseUrl/api/v2/auth/register';
+  //
+}
